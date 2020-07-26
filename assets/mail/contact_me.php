f8ce46d16b9f06f3d42c59000d1635f13ffb12bf
EGA-SUPREMO <cxiesestro@gmail.com>
2020-07-26 16:30:21 -0400

    remove comments

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -1,5 +1,5 @@
 <?php
-// Check for empty fields
+
 if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
   exit();
@@ -9,11 +9,11 @@ $name = strip_tags(htmlspecialchars($_POST['name']));
 $email = strip_tags(htmlspecialchars($_POST['email']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-// Create the email and send the message
-$to = "alejandrogar05@gmail.com"; // Add your email address in between the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
+
+$to = "alejandrogar05@gmail.com"; 
 $subject = "Website Contact Form:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: lol no\n\nMessage:\n$message";
-$header = "From: noreply@testingforlife.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$header = "From: noreply@testingforlife.com\n"; 
 $header .= "Reply-To: $email";	
 
 if(!mail($to, $subject, $body, $header))
