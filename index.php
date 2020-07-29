b97602a5163823be812701a946d6df60a4908b52
Alejandro Garcia <cxiesestro@gmail.com>
2020-07-29 09:34:18 -0400

    Add routing with PHP for multiligual support (#4)
    * init composer
    
    * add routing for multiligual support
    
    * Finish routing system & move index to home.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+error_reporting(E_ERROR | E_PARSE);
+
+$path = $_SERVER['REQUEST_URI'];
+
+define('LOCALE', $path ?? $path == 'en'? 'en' : 'es');
+
+$common = include_once 'assets/lang/'.LOCALE.'/common.php';
+$footer = include_once 'assets/lang/'.LOCALE.'/footer.php';
+$form = include_once 'assets/lang/'.LOCALE.'/form.php';
+$about = include_once 'assets/lang/'.LOCALE.'/about.php';
+$portfolios = [
+    include_once 'assets/lang/'.LOCALE.'/portfolio 1.php',
+    include_once 'assets/lang/'.LOCALE.'/portfolio 2.php',
+    include_once 'assets/lang/'.LOCALE.'/portfolio 3.php',
+    include_once 'assets/lang/'.LOCALE.'/portfolio 4.php',
+    include_once 'assets/lang/'.LOCALE.'/portfolio 5.php',
+    include_once 'assets/lang/'.LOCALE.'/portfolio 6.php',
+];
+
+include_once 'home.php';
