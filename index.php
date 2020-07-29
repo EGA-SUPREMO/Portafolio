fb1ee779d68d2e57d1cfbafc4e177765ca58fdba
EGA-SUPREMO <cxiesestro@gmail.com>
2020-07-28 21:44:24 -0400

    Finish routing system & move index to home.php

--- index.php
+++ index.php
@@ -6,21 +6,19 @@ error_reporting(E_ERROR | E_PARSE);
 
 $path = $_SERVER['REQUEST_URI'];
 
-define('LOCALE', $path?? $path == 'en'? 'en' : 'es');
+define('LOCALE', $path ?? $path == 'en'? 'en' : 'es');
 
-use App\LOCALE;
-
-$common = include_once 'assets/lang/en/common.php';
-$footer = include_once 'assets/lang/en/footer.php';
-$form = include_once 'assets/lang/en/form.php';
-$about = include_once 'assets/lang/en/about.php';
+$common = include_once 'assets/lang/'.LOCALE.'/common.php';
+$footer = include_once 'assets/lang/'.LOCALE.'/footer.php';
+$form = include_once 'assets/lang/'.LOCALE.'/form.php';
+$about = include_once 'assets/lang/'.LOCALE.'/about.php';
 $portfolios = [
-    include_once 'assets/lang/en/portfolio 1.php',
-    include_once 'assets/lang/en/portfolio 2.php',
-    include_once 'assets/lang/en/portfolio 3.php',
-    include_once 'assets/lang/en/portfolio 4.php',
-    include_once 'assets/lang/en/portfolio 5.php',
-    include_once 'assets/lang/en/portfolio 6.php',
+    include_once 'assets/lang/'.LOCALE.'/portfolio 1.php',
+    include_once 'assets/lang/'.LOCALE.'/portfolio 2.php',
+    include_once 'assets/lang/'.LOCALE.'/portfolio 3.php',
+    include_once 'assets/lang/'.LOCALE.'/portfolio 4.php',
+    include_once 'assets/lang/'.LOCALE.'/portfolio 5.php',
+    include_once 'assets/lang/'.LOCALE.'/portfolio 6.php',
 ];
 
 include_once 'home.php';
