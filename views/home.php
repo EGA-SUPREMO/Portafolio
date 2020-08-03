eeb3c7558c9eb1a7e4afa1cf74a990e75bd5664f
EGA-SUPREMO <cxiesestro@gmail.com>
2020-08-03 13:22:08 -0400

    Fix bugs regarding langs selection

--- views/home.php
+++ views/home.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="<?php LOCALE ?>">
+<html lang="<?php echo LOCALE; ?>">
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
